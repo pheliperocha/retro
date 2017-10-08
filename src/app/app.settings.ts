eabfe7ef843958259afde822cc2f520e0e4030ea
Phelipe Rocha <phelipe.rocha@celsolisboa.edu.br>
2017-10-08 11:03:01 -0300

    Class to set app settings

---
+++ src/app/app.settings.ts
@@ -0,0 +1,7 @@
+export class AppSettings {
+  public static INATIVO = 0;
+  public static  PREPARAR = 1;
+  public static  FEEDBACK = 2;
+  public static  REFLEXAO = 3;
+  public static  FINALIZADO = 4;
+}
