5ff01c13d269470580d588b2442b83bcf9db6905
Phelipe Rocha <phelipe.rocha@celsolisboa.edu.br>
2017-10-08 09:55:48 -0300

    Set material design icons and add function to vote on card

--- src/shared/card/card.component.ts
+++ src/shared/card/card.component.ts
@@ -8,6 +8,17 @@ import { Card } from '../../models/card';
 })
 export class CardComponent {
   @Input() card: Card;
+  public voted: boolean = false;
 
   constructor() {}
+
+  vote() {
+    if (this.voted === false) {
+      this.voted = true;
+      this.card.votes++;
+    } else {
+      this.voted = false;
+      this.card.votes--;
+    }
+  }
 }
