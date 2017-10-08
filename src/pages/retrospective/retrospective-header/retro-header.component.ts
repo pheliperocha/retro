787d48cc57ba54bcb946685ac8d54514f363c540
Phelipe Rocha <phelipe.rocha@celsolisboa.edu.br>
2017-10-08 18:01:10 -0300

    Editing Retrospective Title

--- src/pages/retrospective/retrospective-header/retro-header.component.ts
+++ src/pages/retrospective/retrospective-header/retro-header.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {AuthService} from "../../../providers/oauth/auth.service";
 import {User} from "../../../models/user";
+import {Retrospective} from "../../../models/retrospective";
 
 @Component({
   selector: 'app-retre-header',
@@ -9,9 +10,20 @@ import {User} from "../../../models/user";
 })
 export class RetroHeaderComponent {
   public user: User;
+  public editing: boolean = false;
+  @Input() retrospective: Retrospective;
 
   constructor(private authService: AuthService) {
     this.user = this.authService.user;
+  }
+
+  editRetroTitle() {
+    this.editing = true;
+  }
+
+  saveRetroTitle(newTitle: string) {
+    this.retrospective.title = newTitle;
+    this.editing = false;
   }
 
   logout() {
