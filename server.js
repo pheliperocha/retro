eb9c084336b7159e8726d7ece5e85fe1376df9d7
pheliperocha <phelipe.rocha@celsolisboa.edu.br>
2018-06-23 15:34:54 -0300

    Always redirect to Angular front controller

--- server.js
+++ server.js
@@ -5,15 +5,19 @@ var express = require('express');
 
 var app = express();
 
-app.set('port', process.env.PORT || 8080);
+app.set('port', process.env.PORT || 4200);
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var distDir = __dirname + "/dist/";
+let distDir = path.join(__dirname + '/dist/');
 
 app.use(express.static(distDir));
 
 var server = app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
+});
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
