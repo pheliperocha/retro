9445ff31c8a68116a4d251c1900d2bcb53964614
pheliperocha <phelipe.rocha@celsolisboa.edu.br>
2018-06-23 14:57:31 -0300

    Updating address to Heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://ec2-54-94-134-14.sa-east-1.compute.amazonaws.com:5000/',
-  appUrl: 'http://ec2-54-207-6-53.sa-east-1.compute.amazonaws.com/'
+  apiUrl: 'https://retro-api.herokuapp.com/',
+  appUrl: 'https://retro-desk.herokuapp.com/'
 };
